5099fd882b639055cc335b3ea3b4cc5a73606c6c
rahavmagen <rahavm@gmail.com>
2019-03-26 17:56:39 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var webpack = require('webpack');
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+var BUILD_DIR = path.join(__dirname, 'dist');
+var APP_DIR = path.join(__dirname, 'src');
+
+const VENDOR_LIBS = [
+  'react', 'react-dom', 'react-router-dom'
+];
+
+var config = {
+  // entry: APP_DIR + '/index.js',
+  entry: {
+    bundle: APP_DIR + '/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: '[name].[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        // include: APP_DIR,
+        exclude: /node_modules/,
+        // use: 'babel-loader'
+        loader: 'babel-loader',
+        options:{
+          babelrc: false,
+          presets: ["babel-preset-env", "react", "stage-2"],
+          plugins: ['syntax-dynamic-import']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: 'file-loader'
+      }
+    ]
+  },
+  devServer:{
+    contentBase: BUILD_DIR,
+    compress: true,
+    port: 8080,
+    disableHostCheck: false,
+    open: true,
+    hot: true
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+}
+
+module.exports = config;
